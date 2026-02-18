0afa1fb36b905f9096320a634c388eaf2af6eaf3
Ansh Agrawal <ansh909ansh@gmail.com>
2026-02-18 15:31:07 +0530

    daily auto commit

---
+++ daily_git.bat
@@ -0,0 +1,12 @@
+@echo off
+cd /d "C:\Users\ansh9\OneDrive\Documents\daily-github" || (echo Folder not found & pause & exit /b)
+
+echo %date% %time%>> log.txt
+
+git add .
+git commit -m "daily auto commit" >nul 2>&1
+git push
+
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" "https://github.com/DevWithAnsh"
+
+pause
